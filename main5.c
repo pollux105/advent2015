515097169b14deeb96dfde7ce1b9b3bf6f4517f5
c.dutriez <c.dutriez@mercate.fr>
2020-03-11 11:24:34 +0100

    day 5 et 6 complets - prise en compte de l'argument pour 5 et 6

--- main5.c
+++ main5.c
@@ -1,10 +1,52 @@
+/* --- Day 5: Doesn't He Have Intern-Elves For This? ---
+
+Santa needs help figuring out which strings in his text file are naughty or nice.
+
+A nice string is one with all of the following properties:
+
+    It contains at least three vowels (aeiou only), like aei, xazegov, or aeiouaeiouaeiou.
+    It contains at least one letter that appears twice in a row, like xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+    It does not contain the strings ab, cd, pq, or xy, even if they are part of one of the other requirements.
+
+For example:
+
+    ugknbfddgicrmopn is nice because it has at least three vowels (u...i...o...), a double letter (...dd...), and none of the disallowed substrings.
+    aaa is nice because it has at least three vowels and a double letter, even though the letters used by different rules overlap.
+    jchzalrnumimnmhp is naughty because it has no double letter.
+    haegwjzuvuyypxyu is naughty because it contains the string xy.
+    dvszwmarrgswjxmb is naughty because it contains only one vowel.
+
+How many strings are nice?
+
+--- Part Two ---
+
+Realizing the error of his ways, Santa has switched to a better model of determining whether a string is naughty or nice. 
+None of the old rules apply, as they are all clearly ridiculous.
+
+Now, a nice string is one with all of the following properties:
+
+    It contains a pair of any two letters that appears at least twice in the string without overlapping, 
+    like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+    It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+
+For example:
+
+    qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).
+    xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.
+    uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.
+    ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.
+
+How many strings are nice under these new rules?
+*/
+
+
 #include <stdio.h>
 #include <string.h>
 
 #define PART1 1
 #define PART2 2
 
-int main(){
+int main(int argc, char * argv[] ){
 	FILE *data2read;
 	char lastline[20];
 	int compteur = 0;
@@ -16,7 +58,20 @@ int main(){
 	int j = 0;
 	int doubleChar = 0;
 	int doubleCouple = 0;
-	int part = 2;
+	int part;
+	printf("%s",argv[1]);
+	if (!strcmp(argv[1],"part1"))
+	{
+		printf("DAY 5 Partie 1\n");
+		part = PART1;
+	}
+	else
+	{
+		printf("DAY 5 Partie 2\n");
+		part = PART2;
+	}
+
+
 
 	data2read = fopen("data5.txt","r");
 
@@ -36,10 +91,10 @@ int main(){
 					}	
 				}
 	
-				if (consecutiveChars == 0) {printf("CChars BAD\t");}
+/*				if (consecutiveChars == 0) {printf("CChars BAD\t");}
 				else {printf("CChars OK\t");}
 	
-	
+*/	
 				for (j = 0;lastline[j]!='\0';j++)
 				{
 					if (lastline[j] == 'a' || lastline[j] == 'e' || \
@@ -54,11 +109,11 @@ int main(){
 				if (vowelsCount >= 3) 
 				{
 					threeVowels = 1;
-					printf("Vowels OK\t");
+					//printf("Vowels OK\t");
 				}
 				else
 				{
-					printf("Vowels BAD\t");
+					//printf("Vowels BAD\t");
 					//printf("%d\t %s",compteur,lastline);
 				}
 	
@@ -71,25 +126,24 @@ int main(){
 							|| (strstr(lastline,"xy") != NULL))
 				{
 					stringForbidden = 1;
-					printf("Expression interdite\t");
+					//printf("Expression interdite\t");
 				}
 				else
 				{ 
 					stringForbidden = 0;
-					printf("Expression OK   \t");
+					//printf("Expression OK   \t");
 				}
 	
 	
 				if (!stringForbidden && consecutiveChars && threeVowels)
 				{
 					compteur++;
-					printf("Ligne Valide\t%s",lastline);
-					//	break;
+					//printf("Ligne Valide\t%s",lastline);
 				}
 	
 				else
 				{
-					printf("Mauvaise ligne\t%s",lastline);
+					//printf("Mauvaise ligne\t%s",lastline);
 				}	
 
 				threeVowels = 0;
@@ -143,11 +197,11 @@ int main(){
 				if (doubleCouple && doubleChar) 
 				{
 					compteur++;
-					printf("OK\t%s",lastline);
+					//printf("OK\t%s",lastline);
 				}
 				else
 				{
-					printf("BAD\t%s",lastline);
+					//printf("BAD\t%s",lastline);
 				}
 
 			doubleChar = doubleCouple = 0;
@@ -162,7 +216,7 @@ int main(){
 			printf("fichier illisible\n");
 		}
 
-		printf(" Compteur total : %d\n",compteur);
+		printf("Résultat : %d\n",compteur);
 
 		return 0;
 		break;
--- main6.c
+++ main6.c
@@ -1,22 +1,75 @@
+/* 
+ 
+--- Day 6: Probably a Fire Hazard ---
+
+Because your neighbors keep defeating you in the holiday house decorating contest 
+year after year, you've decided to deploy one million lights in a 1000x1000 grid.
+
+Furthermore, because you've been especially nice this year, Santa has mailed you 
+instructions on how to display the ideal lighting configuration.
+
+Lights in your grid are numbered from 0 to 999 in each direction; the lights at 
+each corner are at 0,0, 0,999, 999,999, and 999,0. The instructions include whether 
+to turn on, turn off, or toggle various inclusive ranges given as coordinate pairs. 
+Each coordinate pair represents opposite corners of a rectangle, inclusive; 
+a coordinate pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3 square. 
+The lights all start turned off.
+
+To defeat your neighbors this year, all you have to do is set up your lights by 
+doing the instructions Santa sent you in order.
+
+For example:
+
+    turn on 0,0 through 999,999 would turn on (or leave on) every light.
+    toggle 0,0 through 999,0 would toggle the first line of 1000 lights, 
+    turning off the ones that were on, and turning on the ones that were off.
+    turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights.
+
+After following the instructions, how many lights are lit?
+
+
+--- Part Two ---
+
+You just finish implementing your winning light pattern when you realize you 
+mistranslated Santa's message from Ancient Nordic Elvish.
+The light grid you bought actually has individual brightness controls; each light
+can have a brightness of zero or more. The lights all start at zero.
+The phrase turn on actually means that you should increase the brightness of those lights by 1.
+The phrase turn off actually means that you should decrease the brightness of those lights by 1, 
+to a minimum of zero.
+The phrase toggle actually means that you should increase the brightness of those lights by 2.
+What is the total brightness of all lights combined after following Santa's instructions?
+
+For example:
+
+    turn on 0,0 through 0,0 would increase the total brightness by 1.
+    toggle 0,0 through 999,999 would increase the total brightness by 2000000.
+
+*/
+
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+#define PART1 1
+#define PART2 2
+
+
+
 FILE *data2read;
 char dataline[50];
 int lightRoom[1000][1000];
-struct Corner {
-	int Xpos;
-	int Ypos;
-};
-
-struct Corner corner1;
-struct Corner corner2;
 int compteur = 0;
 int nbOn = 0;
 int nbOff = 0;
 int nbToggle = 0;
+
+
+int part = PART2;
+
+
 
 void toggle(int X1,int Y1, int X2, int Y2)
 {
@@ -51,14 +104,23 @@ void toggle(int X1,int Y1, int X2, int Y2)
 	{
 		for ( j = Ymin; j <= Ymax; j++)
 		{
-			if (lightRoom[i][j] == 0)
-			{
-				lightRoom[i][j] = 1;
+			switch(part){
+				case PART1:
+					if (lightRoom[i][j] == 0)
+					{
+						lightRoom[i][j] = 1;
+					}
+					else
+					{
+						lightRoom[i][j] = 0;
+					}
+				break;
+	
+				case PART2:
+					lightRoom[i][j] +=2;
+				break;
 			}
-			else
-			{
-				lightRoom[i][j] = 0;
-			}
+
 		}
 	}
 }
@@ -97,7 +159,17 @@ void turnOn(int X1,int Y1, int X2, int Y2)
 	{
 		for ( j = Ymin; j <= Ymax; j++)
 		{
-			lightRoom[i][j] = 1;
+			switch (part){
+				case PART1:
+					lightRoom[i][j] = 1;
+				break;
+
+				case PART2:
+					lightRoom[i][j] += 1;
+				break;
+
+			}
+
 		}
 	}
 }
@@ -134,32 +206,53 @@ void turnOff(int X1,int Y1, int X2, int Y2)
 	{
 		for ( j = Ymin; j <= Ymax; j++)
 		{
-			lightRoom[i][j] = 0;
+			switch (part){
+				case PART1:
+					lightRoom[i][j] = 0;
+				break;
+
+				case PART2:
+					lightRoom[i][j] -=1;
+					if (lightRoom[i][j] < 0) lightRoom[i][j] = 0;
+				break;
+			}
 		}
 	}
 }
 
 
 
-int main()
+int main(int argc, char * argv[])
 {
+	printf("%s\n",argv[1]);
+
+	if (!strcmp(argv[1],"part2"))
+	{
+		part = PART2;
+		printf("DAY 6 : PART 2\n");
+	}
+	else
+	{
+		part = PART1;
+		printf("DAY 6 : PART 1\n");
+	}
+
+
 	data2read = fopen("data6.txt","r");
 
 	if (data2read)
 	{
 		while ((fgets(dataline,sizeof(dataline)/sizeof(char),data2read ))!=NULL)
 		{
-			printf("%s\n",dataline);
+			//printf("%s\n",dataline);
 			int i = 0;
 			int coord[4];
 			char *p = dataline;
-			while(*p != 10)
+			while(*p)
 			{
-				printf("%d\n",p);
 				if (isdigit(*p))
 				{
 					coord[i] = strtol(p,&p,10);
-					printf("%d\t",coord[i]);
 					i++;
 				}
 				else
@@ -173,22 +266,22 @@ int main()
 			if(strstr(dataline,"on")!=NULL)
 			{
 				nbOn++;
-				printf("%d\t%d\t%d\t%d\n",coord[1],coord[2],coord[3],coord[4]);
-				turnOn(coord[1],coord[2],coord[3],coord[4]);
+				//printf("%d\t%d\t%d\t%d\n",coord[0],coord[1],coord[2],coord[3]);
+				turnOn(coord[0],coord[1],coord[2],coord[3]);
 			}
 
 			if(strstr(dataline,"off")!=NULL)
 			{
 				nbOff++;
-				printf("%d\t%d\t%d\t%d\n",coord[1],coord[2],coord[3],coord[4]);
-				turnOff(coord[1],coord[2],coord[3],coord[4]);
+				//printf("%d\t%d\t%d\t%d\n",coord[0],coord[1],coord[2],coord[3]);
+				turnOff(coord[0],coord[1],coord[2],coord[3]);
 			}
 
 			if(strstr(dataline,"toggle")!=NULL)
 			{	
 				nbToggle++;
-				printf("%d\t%d\t%d\t%d\n",coord[1],coord[2],coord[3],coord[4]);
-				toggle(coord[1],coord[2],coord[3],coord[4]);
+				//printf("%d\t%d\t%d\t%d\n",coord[0],coord[1],coord[2],coord[3]);
+				toggle(coord[0],coord[1],coord[2],coord[3]);
 			}
 
 
@@ -203,10 +296,19 @@ int main()
 		{
 			for (j=0; j<1000; j++)
 			{	
-				if (lightRoom[i][j] == 1) compteur++;
+				switch(part){
+					case PART1 :
+						if (lightRoom[i][j] == 1) compteur++;
+					break;
+
+					case PART2:
+						compteur += lightRoom[i][j];
+					break;
+				}
 			}
 		}
 	}
+
 	printf("nb on : %d\n",nbOn);
 	printf("nb off : %d\n",nbOff);
 	printf("nb Toggle : %d\n",nbToggle);
