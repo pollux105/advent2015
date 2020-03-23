be94239adbb448cb293ba9df40e69f59e34db3ed
c.dutriez <c.dutriez@mercate.fr>
2020-03-23 18:37:47 +0100

    day7 première partie OK - deuxième aussi moyennant changement dans le fichier

--- main7.c
+++ main7.c
@@ -1,3 +1,51 @@
+/*
+--- Day 7: Some Assembly Required ---
+
+This year, Santa brought little Bobby Tables a set of wires and bitwise logic gates! Unfortunately, little Bobby is a little under the recommended age range, and he needs help assembling the circuit.
+
+Each wire has an identifier (some lowercase letters) and can carry a 16-bit signal (a number from 0 to 65535). A signal is provided to each wire by a gate, another wire, or some specific value. Each wire can only get a signal from one source, but can provide its signal to multiple destinations. A gate provides no signal until all of its inputs have a signal.
+
+The included instructions booklet describes how to connect the parts together: x AND y -> z means to connect wires x and y to an AND gate, and then connect its output to wire z.
+
+For example:
+
+    123 -> x means that the signal 123 is provided to wire x.
+    x AND y -> z means that the bitwise AND of wire x and wire y is provided to wire z.
+    p LSHIFT 2 -> q means that the value from wire p is left-shifted by 2 and then provided to wire q.
+    NOT e -> f means that the bitwise complement of the value from wire e is provided to wire f.
+
+Other possible gates include OR (bitwise OR) and RSHIFT (right-shift). If, for some reason, you'd like to emulate the circuit instead, almost all programming languages (for example, C, JavaScript, or Python) provide operators for these gates.
+
+For example, here is a simple circuit:
+
+123 -> x
+456 -> y
+x AND y -> d
+x OR y -> e
+x LSHIFT 2 -> f
+y RSHIFT 2 -> g
+NOT x -> h
+NOT y -> i
+
+After it is run, these are the signals on the wires:
+
+d: 72
+e: 507
+f: 492
+g: 114
+h: 65412
+i: 65079
+x: 123
+y: 456
+
+In little Bobby's kit's instructions booklet (provided as your puzzle input), what signal is ultimately provided to wire a?
+
+--- Part Two ---
+
+Now, take the signal you got on wire a, override wire b to that signal, and reset the other wires (including wire a). What new signal is ultimately provided to wire a?
+
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -65,8 +113,8 @@ void strip(char *s) {
 
 int wireUnknown(Formula* formula,Wire* tableau)
 {
-    int i;
-
+	int i;
+	printf("%s\n",formula->wire);
     for (i=0 ; i<marqueurEnsemble+1; i++)
     {
         if (!strcmp(formula->wire,tableau[i].identifier))
@@ -149,6 +197,7 @@ void calculateWire(Formula* formula, Wire *tableau, int* ligneE1, int* ligneE2)
 {
     int premier_membre;
     int second_membre;
+    int i;
 
     if (isdigit(formula->identifier1[0]))
     {
@@ -175,6 +224,18 @@ void calculateWire(Formula* formula, Wire *tableau, int* ligneE1, int* ligneE2)
 			strcpy(tableau[marqueurEnsemble].identifier,formula->wire);
 			tableau[marqueurEnsemble].signal = atoi(formula->identifier1);
 		}
+		else
+		{
+			strcpy(tableau[marqueurEnsemble].identifier,formula->wire);
+            		for (i = 0; i<marqueurEnsemble; i++)
+            		{
+                		if (!strcmp(formula->identifier1,tableau[i].identifier))
+                		{
+                	    		tableau[marqueurEnsemble].signal = tableau[i].signal;
+                		}
+            		}
+		}
+
 	}
 	else if (!strcmp(formula->modifier,"NOT"))
 	{
@@ -311,10 +372,9 @@ int main(int argc,char * argv[] )
 
 	int j = 0;
 	int a = 65535;
-	while (j<400)
+	while (strcmp(Ensemble[++j].identifier,"ini"))
 	{
 		printf("identifiant : %s  \tsignal : %d\n",Ensemble[j].identifier,Ensemble[j].signal);
-		j++;
 		if (!strcmp(Ensemble[j].identifier,"a")) a = Ensemble[j].signal;
 	}
 
