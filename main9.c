1664f42b4dbc281cffa345d45138db883babf117
pollux105 <cdutriez@wanadoo.fr>
2020-04-15 21:29:09 +0200

    day9 phase 1 et 2 fini, mais nettoyage à faire

--- main9.c
+++ main9.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int min_distance = 9999;
 struct Travel{
@@ -9,7 +11,9 @@ struct Travel{
 };
 char Chemins[20][20];
 struct Travel Travels[50];
-int nb_villes;
+char *Villes[100];
+char *VillesUniques[50];
+int nb_villes = 1; //la première ville ne peut pas être comparée à elle même.
 int nb_lignes;
 
 int initTravels(struct Travel *Tableau)
@@ -33,35 +37,102 @@ void swap(char *s1,char *s2)
 
 void permute(char *a, int l, int r) 
 { 
-   int i; 
-   if (l == r)
-   {
-	   strcpy(Chemins[i],a);
-	   printf("%s\n", a); 
-   }
-   else
-   { 
-       for (i = l; i <= r; i++) 
-       { 
-          swap((a+l), (a+i)); 
-          permute(a, l+1, r); 
-          swap((a+l), (a+i)); //backtrack 
-       } 
-   } 
+	int i;
+   	int compteur = 0;
+	int x;
+	int y;
+	int nb_chemins = 0;
+	char shortest_chemin[50];
+	int static distance_min = 0;
+	int distance = 0;
+	int comp1 = 0;			//comp1 = 1 si les deux villes comparées sont équivalentes;
+	int comp2 = 0;			//comp2 = 1 si les deux villes comparées sont équivalentes;
+	int comp3 = 0;
+	int comp4 = 0;
+	if (l == r)
+	{
+		printf("%s\n", a);
+		
+		//calcul distance
+		x = 0; y = 0;
+		distance = 0;
+		while(x<strlen(a)-1)
+		{
+			//printf("LIGNE _________ ville[x+1] = %s\n",VillesUniques[a[x+1]-'0']);
+			//printf("LIGNE _________ ville[x+1] = %s\n",VillesUniques[a[x+1]-'0']);
+			//printf("Vil[%d] = %s  \tVil[%d] = %s  \t",x,VillesUniques[a[x]-'0'],x+1,VillesUniques[a[x+1]-'0']);
+			//printf("Ville[%d] = %s\n",x+1,VillesUniques[a[x+1]-'0']);
+			while(1)
+			{
+				comp1 = !strcmp(Travels[y].ville1,VillesUniques[a[x]-'0']);
+				comp2 = !strcmp(Travels[y].ville2,VillesUniques[a[x+1]-'0']);
+				comp3 = !strcmp(Travels[y].ville1,VillesUniques[a[x+1]-'0']);
+				comp4 = !strcmp(Travels[y].ville2,VillesUniques[a[x]-'0']);
+
+				if ( (comp1 == 1 && comp2 == 1) || (comp3 == 1 && comp4 == 1))
+				{
+					distance += Travels[y].dist;
+					y = 0;
+					break;
+				}
+				else 
+				{
+					//printf("y = %d\n",y++);
+					y++;
+				}
+			}
+		
+			//printf("distance = %d\n",distance);
+   			x++;
+		}
+		distance_min = (distance > distance_min) ? distance : distance_min;
+		printf("distance max = %d\n",distance_min);
+
+	}
+	else
+   	{ 
+		for (i = l; i <= r; i++) 
+		{ 
+        		swap((a+l), (a+i)); 
+		        permute(a, l+1, r); 
+        		swap((a+l), (a+i)); //backtrack 
+		} 
+	}
+	
 }
 
 
-int nbUniqueStrElements(char *tab[],char *endstring)
+int nbUniqueStrElements(char *TabFoundStrings[],char *tab[],const char *endstring)
 {
-	int i,j;
 	int i = 0;
 	int j = 0;
-	char *TabFoundStrings[100]; 
-	while (strcmp(tab[i],*endstringfor))
+	int nbElements = 0;
+	int doublon = 0;
+	
+	//char *TabFoundStrings[50]; 
+	while (strcmp(tab[i],endstring))
 	{
-		strcpy(tab[i++],TabFoundStrings[j]
+		j=0;
+		while(j<nbElements)
+		{
+		if (!strcmp(TabFoundStrings[j],tab[i])) doublon = 1;
+			j++;
+		}
+		if (doublon)
+		{
+			i++;
+			doublon = 0;
+		}
+		else
+		{
+			TabFoundStrings[nbElements] = malloc(strlen(tab[i])+1);
+			strcpy(TabFoundStrings[nbElements++],tab[i++]);
+		}
+			
 	}
-
+	TabFoundStrings[nbElements] = malloc(strlen(endstring+1));
+	strcpy(TabFoundStrings[nbElements],endstring);
+	return nbElements;
 }
 
 int main(int argc,char *argv[])
@@ -73,6 +144,7 @@ int main(int argc,char *argv[])
 	data2read = fopen("data9.txt","r");
 	char dataline [100];
 	int i = 0;
+	int j = 0;
 
 
 	while (fgets(dataline,100,data2read) != NULL)
@@ -101,32 +173,61 @@ int main(int argc,char *argv[])
 		nb_lignes++;
 	}
 	
-	for (i = 0;i<nb_lignes-1;i++)
+	for (i = 0;i<nb_lignes;i++)
 	{
-		printf("ville 1 : %s \t ville 2 : %s\t distance : %d\n",Travels[i].ville1,Travels[i].ville2,Travels[i].dist);
+	//	printf("ville 1 : %s \t ville 2 : %s\t distance : %d\n",Travels[i].ville1,Travels[i].ville2,Travels[i].dist);
 	}
 
 	// générer tous les parcours possibles et calculer leur distance
-	// 1. récupérer le nombre de villes
-	i = 1;
+	// 1. récupérer le nombre de villes et générer une tableau contenant une fois chaque ville
+
+	i = 0;
+	j = 0;
 	while (strcmp(Travels[i].ville1,"init"))
 	{
-		if (strcmp(Travels[i].ville1,Travels[i+1].ville1))
-		{
-			nb_villes+=1;
-		}
+		Villes[i] = malloc(strlen(Travels[i].ville1));
+		strcpy(Villes[j],Travels[i].ville1);
+		i++;j++;
+	}
+	i = 0;
+	while (strcmp(Travels[i].ville2,"init"))
+	{
+		Villes[j] = malloc(strlen(Travels[i].ville2));
+		strcpy(Villes[j],Travels[i].ville2);
+
+		i++;j++;
+	}
+		
+	Villes[j] = malloc(50);
+	strcpy(Villes[j],"init");
+
+	nb_villes = nbUniqueStrElements(VillesUniques,Villes,"init");
+	printf("NB VILLEs = %d\n",nb_villes);
+	
+	i=0;
+	while(strcmp(VillesUniques[i],"init"))
+	{
+		printf("ville[%d] : %s\n",i,VillesUniques[i]);
 		i++;
 	}
 
-
-
-
-	printf("nombre de villes differentes = %d\n",nb_villes);
-
+	// 2. générer tous les trajets possibles sur nb_villes 
+	char brassage[50];
+	for (i=0;i<nb_villes;i++)
+	{
+		brassage[i] = '0'+i;
+	}
+	brassage[i]= '\0';
+	permute(brassage,0,strlen(brassage)-1);
 	
-	
 
-//	printf("shortest route = %d",min_distance);
+	//	printf("shortest route = %d",min_distance);
+/*	
+	for (i=0;i<=nb_lignes;i++)
+	{
+		free(Villes[i]);
+	}
+*/
 	return 0;
 
 
--- test.c
+++ test.c
@@ -1,43 +1,54 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 // compte le nombre de chaines uniques dans un tableau
-struct pos{
-	char ville[13];
-	int prix;
-};
 
+char *data[] = {"test1","test2","test3","test7","test4","test4","test7","test6","test6","test7","init"};
+char *end = "init";
 
-
-int cntUStr(char *a, char *str) 
+int nbStringElements(char *tab[],char *endString)
 {
-	int count = 0;	
+	int i = 0;
+	int j = 0;
+	int doublon = 0;
+	int nbElements = 0;
 
-	return count;
-} 
-  
+	char *TabStringFound[50];
+	while(strcmp(tab[i],endString))
+	{
+		j = 0;
+		printf("i = %d\tnbElements = %d\n",i,nbElements);
+		while (j<nbElements)
+		{
+			printf("TabStringFound[%d] = %s\tTab[%d] = %s\n",j,TabStringFound[j],i,tab[i]);
+			if(!strcmp(TabStringFound[j],tab[i]))
+			{
+				doublon = 1;
+				printf("doublon trouvé !!!\n");
+			}
+			j++;
+		}
 
-int main(){
-	struct pos tab[10];
-	strcpy(tab[0].ville,"paris");
-	strcpy(tab[1].ville,"paris");
-	strcpy(tab[2].ville,"paris");
-	strcpy(tab[3].ville,"paris");
-	strcpy(tab[4].ville,"lyon");
-	strcpy(tab[5].ville,"lyon");
-	strcpy(tab[6].ville,"marseille");
-	strcpy(tab[7].ville,"paris");
-	strcpy(tab[8].ville,"toulouse");
-	strcpy(tab[9].ville,"nantes");
+		if (doublon) 	// on va chercher plus loin dans le tableau de données d'entrée
+		{
+			i++;
+			doublon = 0;
+		}
+		else		// on rajoute un élément puis on va chercher plus loin
+		{
+			printf("---------ecriture------\n");
+			TabStringFound[nbElements] = malloc(strlen(tab[i]+1));
+			strcpy(TabStringFound[nbElements++],tab[i++]);
+		}
+	}
 
+	return nbElements;
+}
 
-
-
-
-
-
-
-
-
+int main()
+{
+	printf("nombre d'elements different = %d\n",nbStringElements(data,end));
+	return 0;
 }
 
